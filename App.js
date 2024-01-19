d69d9f196066d1301dece3b0340446779a6bef4d
1351618 <1351618@gmail.com>
2024-01-19 23:07:35 +0200

    test

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!888888888888888</Text>
+      <View>
+        <Text style={styles.qwer}>12</Text>
+      </View>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  qwer: {
+    backgroundColor: "red",
+    height: "40px",
+  },
+});
